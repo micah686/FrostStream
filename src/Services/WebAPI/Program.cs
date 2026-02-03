b6ee72b49f55f1c6d73309c1dc0e021735274a21
micah686 <3616220+micah686@users.noreply.github.com>
2026-02-02 17:58:59 -0800

    Added missing references

--- src/Services/WebAPI/Program.cs
+++ src/Services/WebAPI/Program.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace WebAPI;
 
 public class Program
